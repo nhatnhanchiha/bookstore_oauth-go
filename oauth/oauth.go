b4860ce3dd29dac04ab8f1f537455038670d292d
nhatn <cuocsonglala@gmail.com>
2021-06-17 00:19:31 +0700

    First commit

---
+++ oauth/oauth.go
@@ -0,0 +1,118 @@
+package oauth
+
+import (
+	"fmt"
+	"github.com/go-resty/resty"
+	"github.com/nhatnhanchiha/bookstore_oauth-go/oauth/errors"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+const (
+	headerXPublic   = "X-Public"
+	headerXClientId = "X-Client-Id"
+	headerXCallerId = "X-User-Id"
+
+	paramAccessToken = "access_token"
+)
+
+var (
+	client = resty.New()
+)
+
+type oauthClient struct {
+}
+
+type oauthInterface interface {
+}
+
+type accessToken struct {
+	Id       string `json:"id"`
+	UserId   int64  `json:"user_id"`
+	ClientId int64  `json:"client_id"`
+}
+
+func IsPublic(request *http.Request) bool {
+	if request == nil {
+		return true
+	}
+
+	return request.Header.Get(headerXPublic) == "true"
+}
+
+func GetCallerId(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+
+	callerId, err := strconv.ParseInt(request.Header.Get(headerXCallerId), 10, 64)
+	if err != nil {
+		return 0
+	}
+
+	return callerId
+}
+
+func GetClientId(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+
+	clientId, err := strconv.ParseInt(request.Header.Get(headerXClientId), 10, 64)
+	if err != nil {
+		return 0
+	}
+
+	return clientId
+}
+
+func AuthenticateRequest(request *http.Request) *errors.RestErr {
+	if request == nil {
+		return nil
+	}
+
+	cleanRequest(request)
+
+	accessTokenId := strings.TrimSpace(request.URL.Query().Get(paramAccessToken))
+
+	if accessTokenId == "" {
+		return nil
+	}
+
+	at, err := getAccessToken(accessTokenId)
+	if err != nil {
+		if err.Status == http.StatusNotFound {
+			return nil
+		}
+		return err
+	}
+
+	request.Header.Add(headerXClientId, strconv.FormatInt(at.ClientId, 10))
+	request.Header.Add(headerXCallerId, strconv.FormatInt(at.UserId, 10))
+
+	return nil
+}
+
+func cleanRequest(request *http.Request) {
+	if request == nil {
+		return
+	}
+
+	request.Header.Del(headerXClientId)
+	request.Header.Del(headerXCallerId)
+}
+
+func getAccessToken(accessTokenId string) (*accessToken, *errors.RestErr) {
+	var at accessToken
+	response, err := client.R().SetResult(&at).Get(fmt.Sprintf("http://localhost:8080/oauth/access_token/%s", accessTokenId))
+	if err != nil {
+		return nil, errors.NewInternalServerError(err.Error())
+	}
+
+	if response == nil {
+		return nil, errors.NewInternalServerError("invalid response")
+	}
+
+	return &at, nil
+}
